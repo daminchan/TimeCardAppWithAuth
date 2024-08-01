d7f161556cc7a2742769601f90186a3a4ce9fd9d
daminchan <tetsupo777@gmail.com>
2024-08-01 18:45:56 +0900

    update prisma

--- globals/db.ts
+++ globals/db.ts
@@ -1,23 +1,25 @@
-// import { PrismaClient } from "@prisma/client";
-
-// const globalForPrisma = global as unknown as {
-//   prisma: PrismaClient | undefined;
-// };
-
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClientSingleton | undefined
+// }
+
+// const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
